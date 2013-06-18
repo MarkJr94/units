79aeb28142919fac5c2f4b1cdc8df4c1d9a5718e
=Mark Sinclair <=125axel125@gmail.com>
2013-06-18 13:09:36 -0400

    Adding constants

--- Units.hpp
+++ Units.hpp
@@ -8,39 +8,34 @@
 #include <cstdint>
 #include <iostream>
 
+#ifndef FP_T
+#define FP_T double
+#endif
+
+namespace SI {
+
 template <int length, int mass, int time, int current, int temp, int amount, int lum>
 struct Unit {
 	enum { m =length, kg = mass, s = time, A = current, K = temp, mol = amount, cd = lum};
 };
 
 template <class u1, class u2>
-struct ComputeUnitMul {
+struct UnitMul {
 	typedef Unit<u1::m + u2::m,u1::kg+u2::kg,u1::s+u2::s,u1::A+u2::A,u1::K+u2::K,u1::mol+u2::mol,u1::cd+u2::cd> unit;
 };
 
 template <class u1, class u2>
-struct ComputeUnitDiv {
+struct UnitDiv {
 	typedef Unit<u1::m - u2::m,u1::kg-u2::kg,u1::s-u2::s,u1::A-u2::A,u1::K-u2::K,u1::mol-u2::mol,u1::cd-u2::cd> unit;
 };
 
-template <int sig>
-struct Prefix {
-	enum { magnitude = sig };
-};
-
-template <class Unit, class Prefix = Prefix<0>, class Numeric = double>
+template <class Unit>
 struct Value {
-	explicit Value(Numeric val):
+	constexpr explicit Value(FP_T val):
 		_val(val) {}
 
 
-    template <class P>
-    operator Value<Unit,P,Numeric> ()
-    {
-        return prefix_conv<P>();
-    }
-
-	Numeric val() const { return _val; }
+	FP_T val() const { return _val; }
 
 	Value& operator+=(const Value& rhs)
 	{
@@ -79,74 +74,67 @@ struct Value {
 	}
 
     
-    friend Value& operator*=(Value& lhs, const Numeric rhs)
+    friend Value& operator*=(Value& lhs, const FP_T rhs)
     {
         lhs._val *= rhs;
         return lhs;
     }
 
-    friend Value operator*(Value lhs, const Numeric rhs)
+    friend Value operator*(Value lhs, const FP_T rhs)
     {
         lhs *= rhs;
         return lhs;
     }
 
-    friend Value operator*(const Numeric lhs, Value rhs)
+    friend Value operator*(const FP_T lhs, Value rhs)
     {
         rhs *= lhs;
         return rhs;
     }
 
-    friend Value& operator/=(Value& lhs, const Numeric rhs)
+    friend Value& operator/=(Value& lhs, const FP_T rhs)
     {
         lhs._val /= rhs;
         return lhs;
     }
 
-    friend Value operator/(Value lhs, const Numeric rhs)
+    friend Value operator/(Value lhs, const FP_T rhs)
     {
         lhs /= rhs;
         return lhs;
     }
 
-    friend Value operator/(const Numeric lhs, Value rhs)
+    friend Value operator/(const FP_T lhs, Value rhs)
     {
         rhs /= lhs;
         return rhs;
     }
 
-	template<class U1, class U2, class P, class N>
-	friend auto operator*(const Value<U1,P,N>& lhs, const Value<U2,P,N>& rhs)
-	-> Value<typename ComputeUnitMul<U1,U2>::unit,P,N>;
+	template<class U1, class U2>
+	friend auto operator*(const Value<U1>& lhs, const Value<U2>& rhs)
+	-> Value<typename UnitMul<U1,U2>::unit>;
 
-	template<class U1, class U2,class P, class N>
-	friend auto operator/(const Value<U1,P,N>& lhs, const Value<U2,P,N>& rhs)
-	-> Value<typename ComputeUnitDiv<U1,U2>::unit,P,N>;
-
-	template<class P>
-	constexpr Value<Unit,P,Numeric> prefix_conv()
-	{
-		return Value<Unit,P,Numeric>(_val * pow(10,Prefix::magnitude-P::magnitude));
-	}
-
+	template<class U1, class U2>
+	friend auto operator/(const Value<U1>& lhs, const Value<U2>& rhs)
+	-> Value<typename UnitDiv<U1,U2>::unit>;
 
 private:
-	Numeric _val;
+	FP_T _val;
 };
 
-template<class U1, class U2, class P, class N>
-auto operator*(const Value<U1,P,N>& lhs, const Value<U2,P,N>& rhs)
--> Value<typename ComputeUnitMul<U1,U2>::unit,P,N>
+template<class U1, class U2>
+auto operator*(const Value<U1>& lhs, const Value<U2>& rhs)
+-> Value<typename UnitMul<U1,U2>::unit>
 {
-	typedef Value<typename ComputeUnitMul<U1,U2>::unit,P,N> RetType;
+	typedef Value<typename UnitMul<U1,U2>::unit> RetType;
 	return RetType(lhs._val * rhs._val);
 }
 
-template<class U1, class U2, class P, class N>
-auto operator/(const Value<U1,P,N>& lhs, const Value<U2,P,N>& rhs)
--> Value<typename ComputeUnitDiv<U1,U2>::unit,P,N>
+template<class U1, class U2>
+auto operator/(const Value<U1>& lhs, const Value<U2>& rhs)
+-> Value<typename UnitDiv<U1,U2>::unit>
 {
-	typedef Value<typename ComputeUnitDiv<U1,U2>::unit,P,N> RetType;
+	typedef Value<typename UnitDiv<U1,U2>::unit> RetType;
 	return RetType(lhs._val / rhs._val);
 }
 
@@ -158,28 +146,134 @@ typedef Unit<0,0,0,1,0,0,0> Current;
 typedef Unit<0,0,0,0,1,0,0> Temperature;
 typedef Unit<0,0,0,0,0,1,0> Substance;
 typedef Unit<0,0,0,0,0,0,1> Luminosity;
-
-// Typedefs for metric prefixes
-typedef Prefix<-9> nano;
-typedef Prefix<-6> micro;
-typedef Prefix<-3> milli;
-typedef Prefix<-2> centi;
-typedef Prefix<-1> deci;
-typedef Prefix<0> one;
-typedef Prefix<1> deca;
-typedef Prefix<2> hecto;
-typedef Prefix<3> kilo;
-typedef Prefix<6> mega;
-typedef Prefix<9> giga;
+typedef Unit<0,0,0,0,0,0,0> Unitless;
 
 // Typedefs for basic SI units
-typedef Value<Length> Meter;
-typedef Value<Mass> Kilogram;
-typedef Value<Time> Second;
-typedef Value<Current> Ampere;
-typedef Value<Temperature> Kelvin;
-typedef Value<Substance> Mol;
-typedef Value<Luminosity> Candela;
+using Meter = Value<Length>;
+using Kilogram = Value<Mass>;
+using Second = Value<Time>;
+using Ampere = Value<Current>;
+using Kelvin = Value<Temperature>;
+using Mol = Value<Substance>;
+using Candela = Value<Luminosity>;
+using Scalar = Value<Unitless>;
 
+// Typedefs for more SI measurement types
 
+// Mechanics ============================
+using Area = UnitMul<Length,Length>::unit;
+
+using Volume = UnitMul<Area,Length>::unit;
+
+using Velocity = UnitDiv<Length,Time>::unit;
+
+using Acceleration = UnitDiv<Velocity,Time>::unit;
+
+using Jerk = UnitDiv<Acceleration,Time>::unit;
+
+using Radian = Unitless;
+
+using Force = UnitMul<Mass, Acceleration>::unit;
+using Newton = Value<Force>;
+
+using Energy = UnitMul<Force,Length>::unit;
+using Joule = Value<Energy>;
+
+using Torque = Energy;
+using NewtonMeter = Value<Torque>;
+
+using AngularMomentum = UnitMul<Torque,Time>::unit;
+
+using AngularFrequency = UnitDiv<Radian,Time>::unit;
+
+using AngularAcceleration = UnitDiv<AngularFrequency,Time>::unit;
+
+using Work = Energy;
+
+using Power = UnitDiv<Work,Time>::unit;
+using Watt = Value<Power>;
+
+using Momentum = UnitMul<Mass,Velocity>::unit;
+
+using Pressure = UnitDiv<Force,Area>::unit;
+using Pascal = Value<Pressure>;
+
+using Frequency = UnitDiv<Unitless,Time>::unit;
+using Hertz = Value<Frequency>;
+
+using Intensity = UnitDiv<Power,Area>::unit;
+
+// E&M
+using Charge = UnitMul<Current,Time>::unit;
+using Coulumb = Value<Charge>;
+
+using ElectricFluxDensity = UnitDiv<Charge,Area>::unit;
+
+using Voltage = UnitDiv<Energy,Charge>::unit;
+using Volt = Value<Voltage>;
+
+using ElectricField = UnitDiv<Force,Charge>::unit;
+
+using ElectricalResistance = UnitDiv<Voltage,Current>::unit;
+using Ohm = Value<ElectricalResistance>;
+
+using Capacitance = UnitDiv<Charge,Voltage>::unit;
+using Farad = Value<Capacitance>;
+
+using Permittivity = UnitDiv<Capacitance,Length>::unit;
+
+using Magnetization = UnitDiv<Current,Length>::unit;
+
+using MagneticFlux = UnitMul<Voltage,Time>::unit;
+using Weber = Value<MagneticFlux>;
+
+using MagneticFluxDensity = UnitDiv<MagneticFlux,Area>::unit;
+using Tesla = Value<MagneticFluxDensity>;
+
+// Thermo??
+using Heat = Work;
+
+using HeatCapacity = UnitDiv<Work,Temperature>::unit;
+
+using SpecificHeat = UnitDiv<HeatCapacity,Mass>::unit;
+
+using Entropy = UnitDiv<Energy,Temperature>::unit;
+
+constexpr Meter operator"" m(long double v) {
+    return Meter(v);
+}
+
+constexpr Kilogram operator"" kg(long double v) {
+    return Kilogram(v);
+}
+
+constexpr Second operator"" s(long double v) {
+    return Second(v);
+}
+
+constexpr Ampere operator"" A(long double v) {
+    return Ampere(v);
+}
+
+constexpr Kelvin operator"" K(long double v) {
+    return Kelvin(v);
+}
+
+constexpr Mol operator"" mol(long double v) {
+    return Mol(v);
+}
+
+constexpr Candela operator"" cd(long double v) {
+    return Candela(v);
+}
+
+namespace constants {
+
+    static const auto c = Value<Velocity>(299792458);
+    static const auto G = Value<UnitDiv<UnitDiv<UnitMul<Force,Area>::unit,Mass>::unit,Mass>::unit>(6.67384e-7);
+    static const auto h = Value<UnitMul<Energy,Time>::unit>(6.62606957e-34);
+    static const auto hbar = decltype(h)(1.054571726e-34);
+}
+
+}
 #endif // UNITS_HPP
--- Units_test.cpp
+++ Units_test.cpp
@@ -1,31 +1,37 @@
+#define FP_T double
+
 #include "Units.hpp"
 
 #include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
 int main()
 {
-	typedef Value<Length,kilo> Kilometer;
-	typedef Value<Length,mega> Megameter;
-	typedef ComputeUnitDiv<Length,Time>::unit Velocity;
-    typedef ComputeUnitDiv<Velocity,Time>::unit Acceleration;
-	typedef Value<Velocity,one> MS;
-    typedef Value<Acceleration, one> MpSS;
+    using namespace SI;
+    
+	typedef UnitDiv<Length,Time>::unit Velocity;
+    typedef UnitDiv<Velocity,Time>::unit Acceleration;
+    typedef UnitMul<Mass, Acceleration>::unit Force;
+	
+	typedef Value<Velocity> MpS;
+    typedef Value<Acceleration> MpSS;
+    typedef Value<Force> Newton;
 
-	auto sum = Kilometer(100) + Kilometer(22);
+	auto sum = 100.0m + 22.0m;
 
-	MS velocity = Meter(5) / Second(20);
+	MpS velocity = 5.m / 20.s;
 
-	Meter fall = Meter(MpSS(9.8) * Second(200.4) * Second(200.4))/2;
-	Kilometer kilofall = fall.prefix_conv<kilo>();
-	Megameter megafall = fall.prefix_conv<mega>();
+	Meter fall = velocity * 20.4s + .5 * MpSS(9.8) * 20.4s * 20.4s;
+	
+	Newton grav_force = 100.kg * MpSS(9.8);
 
-	cout << "Value of 100km + 22km = " << sum << endl;
+	cout << "Value of 100m + 22m = " << sum << endl;
 	cout << "Value of 5m / 20s = " << velocity << endl;
-	cout << "Fall distance of an object on earth after 200.4s in meters = " << fall << endl;
-	cout << "Fall distance of an object on earth after 200.4s in kilometers = " << kilofall << endl;
-	cout << "Fall distance of an object on earth after 200.4s in megameters = " << megafall << endl;
+	cout << "Fall distance of an object on earth after 20.4s in meters with initial velocity .25m/s = " << fall << endl;
+	cout << "Underlying type of Value<Unit> is " << typeid(sum.val()).name() << endl;
+	cout << "Gravitational force on 100kg mass = " << grav_force << "N" << endl;
 
 	cout.flush();
 	return 0;
